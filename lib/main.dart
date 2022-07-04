20f9f760e74e57f279654914479c6256920a7ad3
fwdAndroid <fwdkaleem@gmail.com>
2022-07-05 03:59:05 +0500

    Remove Lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:city_max_admin/bottom_pages/test.dart';
 import 'package:city_max_admin/splash/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
