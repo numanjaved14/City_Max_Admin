11b7c766416768d7e738ada4f5bf9631b5cd0aff
numanjaved14 <numanjaved14@gmail.com>
2022-07-18 22:08:37 +0500

    18-7

--- lib/appointments/currentappointments.dart
+++ lib/appointments/currentappointments.dart
@@ -1,8 +1,7 @@
 import 'package:city_max_admin/details/currentappointmentdetails.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class CurrentAppointments extends StatefulWidget {
   const CurrentAppointments({Key? key}) : super(key: key);
@@ -16,57 +15,82 @@ class _CurrentAppointmentsState extends State<CurrentAppointments> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                '29 Decemeber 2002',
-                textAlign: TextAlign.start,
-                style:
-                    TextStyle(color: Colors.black, fontWeight: FontWeight.w900),
-              ),
-            ),
-            StreamBuilder(
-                stream:
-                    FirebaseFirestore.instance.collection('orders').snapshots(),
+        child: FirebaseAuth.instance.currentUser != null
+            ? StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('orders')
+                    .where(
+                      'status',
+                      isEqualTo: 'pending',
+                    )
+                    .snapshots(),
                 builder: (context,
                     AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
                         snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
+                  if (snapshot.hasError) {
                     return const Center(
-                      child: CircularProgressIndicator(),
+                      child: Text('Something went wrong'),
                     );
                   }
-                  return Container(
-                    height: MediaQuery.of(context).size.height,
-                    child: ListView.builder(
-                        itemCount: 5,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Column(
-                            children: [
-                              ListTile(
-                                leading: const Text('12/21/2022'),
-                                trailing: TextButton(
-                                    onPressed: () {
-                                      Navigator.push(
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        // Padding(
+                        //   padding: const EdgeInsets.all(8.0),
+                        //   child: Text(
+                        //     '29 Decemeber 2002',
+                        //     textAlign: TextAlign.start,
+                        //     style:
+                        //         TextStyle(color: Colors.black, fontWeight: FontWeight.w900),
+                        //   ),
+                        // ),
+                        Container(
+                          height: MediaQuery.of(context).size.height,
+                          child: ListView.builder(
+                              itemCount: snapshot.data!.docs.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                Map<String, dynamic> snap =
+                                    snapshot.data!.docs[index].data()
+                                        as Map<String, dynamic>;
+                                return Column(
+                                  children: [
+                                    ListTile(
+                                      onTap: () {
+                                        Navigator.push(
                                           context,
                                           MaterialPageRoute(
-                                              builder: (builder) =>
-                                                  AppointCurrentDetail()));
-                                    },
-                                    child: Text('Detais')),
-                                title: Text("Fawad Kaleem"),
-                                subtitle: Text("234 B Eid Gah Road Lahore"),
-                              ),
-                              Divider()
-                            ],
-                          );
-                        }),
-                  );
-                }),
-          ],
-        ),
+                                            builder: (builder) =>
+                                                AppointCurrentDetail(),
+                                          ),
+                                        );
+                                      },
+                                      leading: Text(snap['date']),
+                                      trailing: TextButton(
+                                        onPressed: () {},
+                                        child: Text(
+                                          'Cancel',
+                                          style: TextStyle(color: Colors.red),
+                                        ),
+                                      ),
+                                      title: Text(snap['serviceType']),
+                                      subtitle: Text(snap['serviceCatgory']),
+                                    ),
+                                    Divider()
+                                  ],
+                                );
+                              }),
+                        ),
+                      ],
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator.adaptive(),
+                    );
+                  }
+                })
+            : const Center(
+                child: Text('Please Register First'),
+              ),
       ),
     );
   }
--- lib/appointments/pastappointments.dart
+++ lib/appointments/pastappointments.dart
@@ -1,7 +1,8 @@
-import 'package:city_max_admin/details/pastappointments.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
+import '../details/currentappointmentdetails.dart';
 
 class PastAppointments extends StatefulWidget {
   const PastAppointments({Key? key}) : super(key: key);
@@ -14,41 +15,84 @@ class _PastAppointmentsState extends State<PastAppointments> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(child: Column(
-          children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text('29 Decemeber 2002',textAlign: TextAlign.start,style: TextStyle(color: Colors.black,fontWeight: FontWeight.w900),),
-                              ),
-
-            Container(
-              height: MediaQuery.of(context).size.height,
-              child: ListView.builder(
-                  itemCount: 5,
-                  itemBuilder: (BuildContext context, int index) {
+      body: SingleChildScrollView(
+        child: FirebaseAuth.instance.currentUser != null
+            ? StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('orders')
+                    .where(
+                      'status',
+                      isNotEqualTo: 'pending',
+                    )
+                    .snapshots(),
+                builder: (context,
+                    AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                        snapshot) {
+                  if (snapshot.hasError) {
+                    return const Center(
+                      child: Text('Something went wrong'),
+                    );
+                  }
+                  if (snapshot.hasData) {
                     return Column(
                       children: [
-                       
-                        ListTile(
-                           onTap: (){
-                                                                                      Navigator.push(context, MaterialPageRoute(builder: (builder) => AppointPastDetail())); 
-
-                           },
-                            leading: const Text('12/21/2022'),
-                            
-                            trailing:   Text('Complete',style: TextStyle(color: Colors.green),),
-                             
-                            
-                            title: Text("Fawad Kaleem"),
-                            subtitle: Text("234 B Eid Gah Road Lahore"),),
-                            Divider()
+                        // Padding(
+                        //   padding: const EdgeInsets.all(8.0),
+                        //   child: Text(
+                        //     '29 Decemeber 2002',
+                        //     textAlign: TextAlign.start,
+                        //     style:
+                        //         TextStyle(color: Colors.black, fontWeight: FontWeight.w900),
+                        //   ),
+                        // ),
+                        Container(
+                          height: MediaQuery.of(context).size.height,
+                          child: ListView.builder(
+                              itemCount: snapshot.data!.docs.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                Map<String, dynamic> snap =
+                                    snapshot.data!.docs[index].data()
+                                        as Map<String, dynamic>;
+                                return Column(
+                                  children: [
+                                    ListTile(
+                                      onTap: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (builder) =>
+                                                AppointCurrentDetail(),
+                                          ),
+                                        );
+                                      },
+                                      leading: Text(snap['date']),
+                                      trailing: TextButton(
+                                        onPressed: () {},
+                                        child: Text(
+                                          'Cancel',
+                                          style: TextStyle(color: Colors.red),
+                                        ),
+                                      ),
+                                      title: Text(snap['serviceType']),
+                                      subtitle: Text(snap['serviceCatgory']),
+                                    ),
+                                    Divider()
+                                  ],
+                                );
+                              }),
+                        ),
                       ],
                     );
-                        
-                  }),
-            ),
-          ],
-        ),),
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator.adaptive(),
+                    );
+                  }
+                })
+            : const Center(
+                child: Text('Please Register First'),
+              ),
+      ),
     );
   }
 }
