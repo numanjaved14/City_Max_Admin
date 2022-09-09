cb5539190e58c4cbe045b3de05ff9eaf40b302f7
Numan Javed <40382065+numanjaved14@users.noreply.github.com>
2022-09-09 20:57:58 +0500

    Final Release 9-9-22

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
@@ -8,16 +7,20 @@ import 'package:image_picker/image_picker.dart';
 //Image Picker Code
 pickImage(ImageSource source) async {
   final ImagePicker imagePicker = ImagePicker();
-  XFile ? file = await imagePicker.pickImage(source: source);
+  XFile? file = await imagePicker.pickImage(source: source);
 
-  if(file != null){
+  if (file != null) {
     return await file.readAsBytes();
   }
   print('No Image Selected');
 }
 
-
 /// SnakBar Code
-showSnakBar(String contexts,BuildContext context){
- ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(contexts)));
+showSnakBar(String contexts, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(contexts)));
+}
+
+class AlwaysDisabledFocusNode extends FocusNode {
+  @override
+  bool get hasFocus => false;
 }
