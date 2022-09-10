66e9799f40cb2d5d6c7086c59eebc9c90f3e2837
fwdAndroid <fwdkaleem@gmail.com>
2022-09-10 11:57:08 +0500

    Final Change Bottom Set

--- lib/main/mainscreen.dart
+++ lib/main/mainscreen.dart
@@ -1,12 +1,9 @@
-import 'dart:ui';
-
 import 'package:city_max_admin/bottom_pages/home_page.dart';
 import 'package:city_max_admin/bottom_pages/service_page.dart';
 import 'package:city_max_admin/bottom_pages/user_appointment_page.dart';
 import 'package:city_max_admin/bottom_pages/userlist.dart';
 import 'package:city_max_admin/discount/discount_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -15,8 +12,7 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   // Properties & Variables needed
-  PersistentTabController _controller =
-      PersistentTabController(initialIndex: 0);
+
   int currentTab = 0; // to keep track of active tab index
   final List<Widget> screens = [
     MyHomePage(),
@@ -25,91 +21,138 @@ class _MainScreenState extends State<MainScreen> {
     NotificationScreen(),
     DiscountScreen(),
   ]; // to store nested tabs
+  final PageStorageBucket bucket = PageStorageBucket();
+  Widget currentScreen = MyHomePage(); // Our first view in viewport
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: PersistentTabView(
-      controller: _controller,
-      screens: screens,
-      backgroundColor: Colors.white, // Default is Colors.white.
-      handleAndroidBackButtonPress: true, // Default is true.
-      resizeToAvoidBottomInset:
-          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-      stateManagement: true, // Default is true.
-      hideNavigationBarWhenKeyboardShows:
-          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
-      decoration: NavBarDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        colorBehindNavBar: Colors.white,
+      body: PageStorage(
+        child: currentScreen,
+        bucket: bucket,
       ),
-      popAllScreensOnTapOfSelectedTab: true,
-      popActionScreens: PopActionScreensType.all,
-      itemAnimationProperties: ItemAnimationProperties(
-        // Navigation Bar's items animation properties.
-        duration: Duration(milliseconds: 200),
-        curve: Curves.ease,
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Container(
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              MaterialButton(
+                minWidth: 40,
+                onPressed: () {
+                  setState(() {
+                    currentScreen =
+                        MyHomePage(); // if user taps on this dashboard tab will be active
+                    currentTab = 0;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/home.png',
+                      height: 30,
+                      width: 30,
+                    ),
+                    Text("Home")
+                  ],
+                ),
+              ),
+              //Service
+              MaterialButton(
+                minWidth: 40,
+                onPressed: () {
+                  setState(() {
+                    currentScreen = ServicePage();
+                    // if user taps on this dashboard tab will be active
+                    currentTab = 1;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/service.png',
+                      height: 30,
+                      width: 30,
+                    ),
+                    Text("Service")
+                  ],
+                ),
+              ),
+
+              //Appointment
+              MaterialButton(
+                minWidth: 40,
+                onPressed: () {
+                  setState(() {
+                    currentScreen = ServicePage();
+                    // if user taps on this dashboard tab will be active
+                    currentTab = 1;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/deadline.png',
+                      height: 30,
+                      width: 30,
+                    ),
+                    Text("Appointment")
+                  ],
+                ),
+              ),
+              MaterialButton(
+                minWidth: 40,
+                onPressed: () {
+                  setState(() {
+                    currentScreen =
+                        NotificationScreen(); // if user taps on this dashboard tab will be active
+                    currentTab = 2;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/notification.png',
+                      height: 30,
+                      width: 30,
+                    ),
+                    Text("Notification")
+                  ],
+                ),
+              ),
+
+              ///Create Discount
+              MaterialButton(
+                minWidth: 40,
+                onPressed: () {
+                  setState(() {
+                    currentScreen =
+                        DiscountScreen(); // if user taps on this dashboard tab will be active
+                    currentTab = 4;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/discount.png',
+                      height: 30,
+                      width: 30,
+                    ),
+                    Text("Discount")
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-      screenTransitionAnimation: ScreenTransitionAnimation(
-        // Screen transition animation on change of selected tab.
-        animateTabTransition: true,
-        curve: Curves.ease,
-        duration: Duration(milliseconds: 200),
-      ),
-      context,
-      // items: [],
-      items: [
-        PersistentBottomNavBarItem(
-          icon: Image.asset(
-            'assets/home.png',
-            height: 30,
-            width: 30,
-          ),
-          title: ("Home"),
-          activeColorPrimary: Color(0xff0DC6DF),
-          inactiveColorPrimary: Color(0xff8d8282),
-        ),
-        PersistentBottomNavBarItem(
-          icon: Image.asset(
-            'assets/service.png',
-            height: 30,
-            width: 30,
-          ),
-          title: ("Services"),
-          activeColorPrimary: Color(0xff0DC6DF),
-          inactiveColorPrimary: Color(0xff8d8282),
-        ),
-        PersistentBottomNavBarItem(
-          icon: Image.asset(
-            'assets/deadline.png',
-            height: 30,
-            width: 30,
-          ),
-          title: ("Appointment"),
-          activeColorPrimary: Color(0xff0DC6DF),
-          inactiveColorPrimary: Color(0xff8d8282),
-        ),
-        PersistentBottomNavBarItem(
-          icon: Image.asset(
-            'assets/notification.png',
-            height: 30,
-            width: 30,
-          ),
-          title: ("Notification"),
-          activeColorPrimary: Color(0xff0DC6DF),
-          inactiveColorPrimary: Color(0xff8d8282),
-        ),
-        PersistentBottomNavBarItem(
-          icon: Image.asset(
-            'assets/discount.png',
-            height: 30,
-            width: 30,
-          ),
-          title: ("Discount"),
-          activeColorPrimary: Color(0xff0DC6DF),
-          inactiveColorPrimary: Color(0xff8d8282),
-        ),
-      ],
-    ));
+    );
   }
 }
